bbbd57294ffccf355d2077037f6b37c72e02df0d
unknown <everythingfree4200@gmail.com>
2025-03-03 13:54:57 +0530

    first commit

---
+++ db/Database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const DB_URL = 'mongodb+srv://everythingfree4200:anshsharma@21@cluster0.1ivdd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((data) => {
+      console.log(`mongod connected with server: ${data.connection.host}`);
+    });
+};
+
+module.exports = connectDatabase;
