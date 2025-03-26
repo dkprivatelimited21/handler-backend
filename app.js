7cdfc66eab190f0a72d5153d1ea1792cba18b78f
unknown <everythingfree4200@gmail.com>
2025-03-26 22:45:18 +0530

    cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require('cors')
 const corsOption = {
-    origin: ['https://local-handler.vercel.app/'],
+    origin: ["https://local-handler.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }
