217fec1a95d2d5c4eeeaf7513203e5b878ab5ba8
unknown <everythingfree4200@gmail.com>
2025-03-31 14:03:21 +0530

    25.0

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+import
+
 const express = require("express");
 const ErrorHandler = require("./middleware/error");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const cors = require("cors")
+
+app.use(
+	cors({
+	   origin:"https://local-handler.vercel.app",
+	   method:["GET","POST"],
+})
+)
+
 
 app.use(express.json());
 app.use(cookieParser());
