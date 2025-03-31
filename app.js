5e25356c5b5aabba88284197116ebb9de1d8cb04
unknown <everythingfree4200@gmail.com>
2025-03-31 21:23:42 +0530

    36.0

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
     res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
     next(); 
-})
+});
 app.use(express.json());
 app.use(cookieParser());
 app.use("/test", (req, res) => {
