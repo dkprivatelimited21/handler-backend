838ca6b94e3bc923d24312a12dd4755e66b07ea2
unknown <everythingfree4200@gmail.com>
2025-04-15 22:57:35 +0530

    9

--- controller/payment.js
+++ controller/payment.js
@@ -3,15 +3,22 @@ const router = express.Router();
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const Razorpay = require("razorpay");
 
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_SECRET,
+});
+
+// Stripe payment
 router.post(
   "/process",
   catchAsyncErrors(async (req, res, next) => {
     const myPayment = await stripe.paymentIntents.create({
       amount: req.body.amount,
-      currency: "inr",
+      currency: "INR",
       metadata: {
-        company: "Becodemy",
+        company: "dkprivatelimited21",
       },
     });
     res.status(200).json({
@@ -21,6 +28,7 @@ router.post(
   })
 );
 
+// Stripe API key
 router.get(
   "/stripeapikey",
   catchAsyncErrors(async (req, res, next) => {
@@ -28,5 +36,19 @@ router.get(
   })
 );
 
+// Razorpay order route
+router.post(
+  "/razorpay-checkout",
+  catchAsyncErrors(async (req, res) => {
+    const options = {
+      amount: req.body.amount,
+      currency: "INR",
+      receipt: `receipt_order_${Date.now()}`,
+    };
+
+    const order = await razorpay.orders.create(options);
+    res.status(200).json(order);
+  })
+);
 
 module.exports = router;
