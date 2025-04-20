0f813313d8b7e98e8b0ab23e925c639c1291ca8a
unknown <everythingfree4200@gmail.com>
2025-04-20 23:12:35 +0530

    000

--- model/order.js
+++ model/order.js
@@ -1,67 +1,56 @@
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-    cart: [
-  {
-    productId: {
-      type: String,
-      required: true,
+  cart: [
+    {
+      productId: { type: String, required: true },
+      name: { type: String },
+      quantity: { type: Number, required: true },
+      price: { type: Number },
+      selectedSize: { type: String },
+      selectedColor: { type: String },
+      image: { type: String },
+      shopId: { type: String },
+      isReviewed: { type: Boolean, default: false },
+      trackingId: { type: String, default: null },
     },
-    name: {
-      type: String,
-    },
-    quantity: {
-      type: Number,
-      required: true,
-    },
-    price: {
-      type: Number,
-    },
-    selectedSize: {
-      type: String,
-    },
-    selectedColor: {
-      type: String,
-    },
-    image: {
-      type: String,
-    },
-    shopId: {
-      type: String,
-    },
-    isReviewed: {
-      type: Boolean,
-      default: false,
-    },
-	trackingId: {
-  	type: String,
-  	default: null,
-    },
+  ],
+
+  shippingAddress: {
+    address1: { type: String },
+    address2: { type: String },
+    zipCode: { type: String },
+    country: { type: String },
+    city: { type: String },
   },
-],
 
-    paymentInfo:{
-        id:{
-            type: String,
-        },
-        status: {
-            type: String,
-        },
-        type:{
-            type: String,
-        },
-    },
-    paidAt:{
-        type: Date,
-        default: Date.now(),
-    },
-    deliveredAt: {
-        type: Date,
-    },
-    createdAt:{
-        type: Date,
-        default: Date.now(),
-    },
+  user: {
+    _id: { type: String },
+    name: { type: String },
+    email: { type: String },
+  },
+
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+
+  paymentInfo: {
+    id: { type: String },
+    status: { type: String },
+    type: { type: String },
+  },
+
+  paidAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  deliveredAt: {
+    type: Date,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
-
 module.exports = mongoose.model("Order", orderSchema);
