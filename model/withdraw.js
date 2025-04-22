a043ddb244215bc272f5d06d880da060d0834353
unknown <everythingfree4200@gmail.com>
2025-04-22 22:35:48 +0530

    0

--- model/withdraw.js
+++ model/withdraw.js
@@ -1,25 +1,26 @@
 const mongoose = require("mongoose");
 
-const withdrawSchema = new mongoose.Schema({
-  seller: {
-    type: Object,
-    required: true,
+const withdrawSchema = new mongoose.Schema(
+  {
+    seller: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Shop",  // Referencing the 'Shop' model
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    serviceCharge: {  // Adding service charge field
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "Processing",
+    },
   },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  status: {
-    type: String,
-    default: "Processing",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  updatedAt:{
-    type: Date,
-  }
-});
+  { timestamps: true }  // Automatically handles createdAt and updatedAt
+);
 
 module.exports = mongoose.model("Withdraw", withdrawSchema);
